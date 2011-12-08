8585e10dc864f427c4b8090d815f404ee16b2473
Jared Hanson <jaredhanson@gmail.com>
2011-12-08 08:34:14 -0800

    Initial commit.

---
+++ test/index-test.js
@@ -0,0 +1,15 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var angellist = require('passport-angellist');
+
+
+vows.describe('passport-angellist').addBatch({
+  
+  'module': {
+    'should report a version': function (x) {
+      assert.isString(angellist.version);
+    },
+  },
+  
+}).export(module);
---
+++ test/strategy-test.js
@@ -0,0 +1,138 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var AngelListStrategy = require('passport-angellist/strategy');
+
+
+vows.describe('AngelListStrategy').addBatch({
+  
+  'strategy': {
+    topic: function() {
+      return new AngelListStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+    },
+    
+    'should be named angellist': function (strategy) {
+      assert.equal(strategy.name, 'angellist');
+    },
+  },
+  
+  'strategy when loading user profile': {
+    topic: function() {
+      var strategy = new AngelListStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+      
+      // mock
+      strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
+        var body = '{ \
+          "name": "Naval Ravikant", \
+          "id": 155, \
+          "bio": "Sweat - AngelList, Venture Hacks, Vast, Epinions; Money - Twitter, Heyzap, SnapLogic", \
+          "blog_url": "http://startupboy.com/", \
+          "online_bio_url": "http://startupboy.com/about/", \
+          "twitter_url": "http://twitter.com/naval", \
+          "facebook_url": "http://www.facebook.com/navalr", \
+          "linkedin_url": "http://www.linkedin.com/in/navalr", \
+          "follower_count": 13375, \
+          "locations": [ \
+            { \
+              "id": 1692, \
+              "tag_type": "LocationTag", \
+              "name": "san francisco", \
+              "display_name": "San Francisco", \
+              "angellist_url": "http://angel.co/san-francisco" \
+            } \
+          ], \
+          "roles": [ \
+            { \
+              "id": 9305, \
+              "tag_type": "RoleTag", \
+              "name": "seed funds", \
+              "display_name": "Seed Fund", \
+              "angellist_url": "http://angel.co/seed-funds" \
+            }, \
+            { \
+              "id": 14725, \
+              "tag_type": "RoleTag", \
+              "name": "entrepreneur", \
+              "display_name": "Entrepreneur", \
+              "angellist_url": "http://angel.co/entrepreneur-1" \
+            } \
+          ], \
+          "angellist_url": "http://angel.co/naval", \
+          "image": "https://s3.amazonaws.com/photos.angel.co/users/155-medium?1308634544" \
+        }';
+        
+        callback(null, body, undefined);
+      }
+      
+      return strategy;
+    },
+    
+    'when told to load user profile': {
+      topic: function(strategy) {
+        var self = this;
+        function done(err, profile) {
+          self.callback(err, profile);
+        }
+        
+        process.nextTick(function () {
+          strategy.userProfile('access-token', done);
+        });
+      },
+      
+      'should not error' : function(err, req) {
+        assert.isNull(err);
+      },
+      'should load profile' : function(err, profile) {
+        assert.equal(profile.provider, 'angellist');
+        assert.equal(profile.id, '155');
+        assert.equal(profile.displayName, 'Naval Ravikant');
+      },
+    },
+  },
+  
+  'strategy when loading user profile and encountering an error': {
+    topic: function() {
+      var strategy = new AngelListStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      },
+      function() {});
+      
+      // mock
+      strategy._oauth2.getProtectedResource = function(url, accessToken, callback) {
+        callback(new Error('something-went-wrong'));
+      }
+      
+      return strategy;
+    },
+    
+    'when told to load user profile': {
+      topic: function(strategy) {
+        var self = this;
+        function done(err, profile) {
+          self.callback(err, profile);
+        }
+        
+        process.nextTick(function () {
+          strategy.userProfile('access-token', done);
+        });
+      },
+      
+      'should error' : function(err, req) {
+        assert.isNotNull(err);
+      },
+      'should not load profile' : function(err, profile) {
+        assert.isUndefined(profile);
+      },
+    },
+  },
+  
+}).export(module);
